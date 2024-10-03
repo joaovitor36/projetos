97cf825ff9ecaefc9722a77ae4bd67218a074d76
João Vitor <129603128+joaovitor36@users.noreply.github.com>
2024-10-03 15:25:09 -0300

    mudando algumas coisas

---
+++ login/index.js
@@ -0,0 +1,20 @@
+/*// Importar as bibliotecas necessárias
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+// Usar o body-parser para interpretar dados de formulários
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
+var path = require('path');
+// Criar a aplicação express
+const app = express();
+
+// Iniciar o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});*/
+
+
+
